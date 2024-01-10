c75e29c3bd1faaabcb8433856c1b65391de33450
A253875 <A253875@LSBSA-65LG3J3>
2024-01-10 16:22:01 +0200

    Add project files.

---
+++ 4.Data types/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Thabile";
+            int age = 10;
+            bool isTall = false;
+            double weight = 25.5;
+
+            //Hi Thabile, you are 10 and your weight is 25.5
+            Console.WriteLine(" Hi " + name + ", you are " + age + " and your weight is " + weight);
+            
+            //
+            Console.WriteLine(" true or false you are tall: " + isTall);
+        }
+    }
+}
